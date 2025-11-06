d48984704003f53dfa42212edf1e85edfd6ee974
Jarell E. Portillas <2201107699@student.buksu.edu.ph>
2025-11-06 19:03:59 +0800

    lol wla na sugdan

---
+++ cleint/weatherupdate/src/router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+    }
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true
+    }
+  }
+);
